fba39a7b308be282dc62b077bcd3cc70e3962311
vijayganesh <vijayganeshpc@gmail.com>
2022-03-03 23:23:45 +0530

    Skelton of RISC V 32 E

---
+++ DLPRV32/RISCV32.v
@@ -0,0 +1,14 @@
+/* Main RISC V Core file */ 
+
+
+module DRISCV(
+  
+);
+  input clk,Din;
+  output Dout;
+  // Above is general input and output need to define properly 
+  
+  // Call for control, Memory and ALU modules from here
+  
+  
+endmodule
