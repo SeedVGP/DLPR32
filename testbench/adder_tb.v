47bf9ea8d632ed341ed256a4332f21c403504b37
Sujan Tendulkar <73121109+9606sujan@users.noreply.github.com>
2022-07-18 12:19:58 +0530

    Update adder_tb.v

--- testbench/adder_tb.v
+++ testbench/adder_tb.v
@@ -1,24 +1,17 @@
+
+----------------------Test Bench-------------------------------
 `timescale 1ns/1ps 
-module addk_tb(); 
-reg clk,cs_add,rst;
-  reg [15:0] x,y;
+module addq_tb(); 
+reg rst;
+reg [15:0] x,y;
 wire [15:0] sum;
-wire rdy_add;
-  addk addd(clk,cs_add,rst,x,y,sum,rdy_add);
-initial
-begin
-clk=1'b0;
-forever #5 clk=~clk;
-end
+  addq addd(rst,x,y,sum);
+
 initial
 begin
 rst=1;
-#5
-cs_add=0;
 #10;
 rst=0;
-cs_add=1;
-
 x=16'b1011010000000000;
 y=16'b1011010000000000;
 #50;
@@ -26,3 +19,4 @@ $finish;
 end
 
 endmodule
+
